d63b4ea0d00b13f268c95a980c61c2926c061b84
nils <nils.kaerger@gmx.de>
2021-04-01 11:07:49 +0200

    Reader2

--- PIC16F84 Simulator/src/Read/ReadLST.java
+++ PIC16F84 Simulator/src/Read/ReadLST.java
@@ -1,37 +1,40 @@
 package Read;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.nio.file.Files;
 import java.io.File;
 
-public class ReadLST{
+public class ReadLST {
 
     public File data;
     public Scanner scan;
-    public String content="";
+    public static int counter = 0;
+    public ArrayList<String> liste = new ArrayList<String>();
 
-    public ReadLST(String path){
+    public ReadLST(String path) {
 
         this.data = new File(path);
 
     }
 
-    public Scanner initializeScanner() throws FileNotFoundException{
+    public Scanner initializeScanner() throws FileNotFoundException {
         this.scan = new Scanner(this.data);
         return this.scan;
     }
 
-    public String readFile(){
-        while(this.scan.hasNextLine()){
-            this.content = content.concat(this.scan.nextLine() + "\n");
+    public ArrayList<String> readFile() {
+
+        while (this.scan.hasNextLine()) {
+            String line = scan.nextLine();
+            liste.add(line);
+
         }
 
-        return this.content;
+        return liste;
     }
 
-
-
-
-
-
     
+
 }
--- PIC16F84 Simulator/src/Read/tst.java
+++ PIC16F84 Simulator/src/Read/tst.java
@@ -1,14 +1,16 @@
 package Read;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class tst {
 
     public static void main(String[] args) throws FileNotFoundException{
-        ReadLST list = new ReadLST("C:/Users/User/Documents/hi.odt");
+        ReadLST list = new ReadLST("C:/Users/User/Documents/test.txt");
         list.initializeScanner();
-        String liste = list.readFile();
-        System.out.println(liste);
+        ArrayList<String> liste = new ArrayList<String>();
+        liste = list.readFile();
+        System.out.println(liste.get(10));
     }
     
 }
